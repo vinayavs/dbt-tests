3df84835151a233804c1a1a6e2b2c933378f05e7
Vinay AVS <65980562+vinayavs@users.noreply.github.com>
2022-03-23 13:44:43 +0530

    Delete test.py

--- learn-dbt/test.py
+++
@@ -1,9 +0,0 @@
-import json
-import subprocess as sp 
-cmd="aws iam list-users --no-paginate"
-out_err=sp.Popen(cmd, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
-rt=out_err.wait()
-out,err=out_err.communicate()
-print(json.loads(out))
-print("***********")
-print(err)
